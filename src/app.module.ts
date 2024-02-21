1733de9d4f603555bdbab8d4eed6dcc1266a282a
vinicius <viniciusvwh1441@gmail.com>
2024-02-21 17:56:09 -0300

    feat: auth controller & envs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateAccountController } from './controllers/create-acoount.controller'
+import { envSchema } from './env'
 import { PrismaService } from './prisma/prisma.service'
+import { AuthModule } from './src/auth.module'
 
 @Module({
-  controllers: [CreateAccountController],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    AuthModule,
+  ],
+  controllers: [CreateAccountController, AuthenticateController],
   providers: [PrismaService],
 })
 export class AppModule {}
